d0cf431e4f11912459da21e3bbf3df6a06e83ff4
Emily Menchu <menchu.lucila@gmail.com>
2025-06-10 09:57:20 -0600

    LOGRO: CREACION DE ENTIDADES Y SUS RELACIONES

--- src/main/java/com/emilymenchu/projects/MovieManagement/MovieManagementApplication.java
+++ src/main/java/com/emilymenchu/projects/MovieManagement/MovieManagementApplication.java
@@ -1,4 +1,4 @@
-package com.emilymenchu.MovieManagement;
+package com.emilymenchu.projects.MovieManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
