a6a2408b7ff009540d99ed74a78b4be98448a546
Emily Menchu <menchu.lucila@gmail.com>
2025-06-17 11:17:36 -0600

    LOGRO: APRENDER A CONFIGURAR OBJETOS PAGEABLE Y SORT DE FORMA AUTOMATICA EN LA PETICION REST Y APRENDER A UTILIZAR PAGINACION Y ORDENAMIENTO CON KEYWORD 'ORDERBY' Y CLASE SORT E INTERFAZ PAGEABLE

--- src/main/java/com/emilymenchu/projects/MovieManagement/MovieManagementApplication.java
+++ src/main/java/com/emilymenchu/projects/MovieManagement/MovieManagementApplication.java
@@ -1,7 +1,14 @@
 package com.emilymenchu.projects.MovieManagement;
 
+import com.emilymenchu.projects.MovieManagement.persistence.repository.MovieCrudRepository;
+import com.emilymenchu.projects.MovieManagement.util.MovieGenre;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @SpringBootApplication
 public class MovieManagementApplication {
@@ -10,4 +17,12 @@ public class MovieManagementApplication {
 		SpringApplication.run(MovieManagementApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner testFindAllMOviesByGenreOrderBy(MovieCrudRepository movieCrudRepository) {
+		return args -> {
+			Sort sort = Sort.by("releaseYear").descending().and(Sort.by(Sort.Direction.ASC, "id"));
+			Pageable pageable = PageRequest.of(0, 10, sort);
+			movieCrudRepository.findAllByGenre(MovieGenre.DRAMA, pageable).forEach(System.out::println);
+		};
+	}
 }
--- src/main/java/com/emilymenchu/projects/MovieManagement/persistence/repository/MovieCrudRepository.java
+++ src/main/java/com/emilymenchu/projects/MovieManagement/persistence/repository/MovieCrudRepository.java
@@ -3,10 +3,13 @@ package com.emilymenchu.projects.MovieManagement.persistence.repository;
 import com.emilymenchu.projects.MovieManagement.persistence.entity.Movie;
 import com.emilymenchu.projects.MovieManagement.util.MovieGenre;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
 public interface MovieCrudRepository extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {
+    Page<Movie> findAllByGenre(MovieGenre genre, Pageable pageable);
 }
